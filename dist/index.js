b096acda241a31f3dfa0493e754a237aefd3fbee
AlexRob <alex.rob@gmx.at>
2017-11-15 09:47:37 +0100

    First commit

---
+++ dist/index.js
@@ -0,0 +1,69 @@
+// Prepare Form
+$("#otherMediaChannel").hide();
+validateSubmit();
+// Validation
+$("#firstName").keyup(function () {
+    if ($(this).val()) {
+        $("#firstNameMandatory").hide();
+        validateSubmit();
+    }
+    else {
+        $("#firstNameMandatory").show();
+    }
+});
+$("#lastName").keyup(function () {
+    if ($(this).val()) {
+        $("#lastNameMandatory").hide();
+        validateSubmit();
+    }
+    else {
+        $("#lastNameMandatory").show();
+    }
+});
+$("#emailMandatory").hide();
+$("#email").keyup(function () {
+    if ($(this).val()) {
+        $("#emailMandatory").hide();
+        validateSubmit();
+    }
+    else {
+        if ($("#newsletter").prop('checked')) {
+            $("#emailMandatory").show();
+        }
+    }
+});
+$("#newsletter").change(function () {
+    if ($(this).prop('checked')) {
+        if (!$("#email").val()) {
+            $("#emailMandatory").show();
+        }
+    }
+    else {
+        $("#emailMandatory").hide();
+    }
+    validateSubmit();
+});
+$("#mediaChannelSelect").change(function () {
+    if ($(this).val() === "Other") {
+        $("#otherMediaChannel").show();
+    }
+    else {
+        $("#otherMediaChannel").hide();
+    }
+});
+function validateSubmit() {
+    /*
+    // I tried to find out whether all children of the alert class are visible or not, but unfortunately it doesn't work at all
+    if($("div.alert").children(':visible').length == 0) {
+        $("button.btn").attr("enabled", "enabled");
+    } else {
+        $("button.btn").attr("disabled", "disabled");
+    }
+    */
+    if ($("#firstNameMandatory").is(":hidden") && $("#lastNameMandatory").is(":hidden") && $("#emailMandatory").is(":hidden")) {
+        $("button.btn").removeAttr("disabled");
+    }
+    else {
+        $("button.btn").attr("disabled", "disabled");
+    }
+}
